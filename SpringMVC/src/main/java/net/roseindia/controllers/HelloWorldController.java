75ab3c425516507c5cb59a97dc434e7a1be09f6e
lbeles <lbeles@gs83.goldmann.sk>
2012-11-22 17:34:30 +0100

    dd

---
+++ SpringMVC/src/main/java/net/roseindia/controllers/HelloWorldController.java
@@ -0,0 +1,16 @@
+package net.roseindia.controllers;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+	@RequestMapping("/helloworld")
+	   public ModelAndView helloWord(){
+		  System.out.println("ZZZZ");
+	      String message = "Hello World, Spring 3.0!";
+		  return new ModelAndView("helloworld", "message",message);
+	   }
+}
+
